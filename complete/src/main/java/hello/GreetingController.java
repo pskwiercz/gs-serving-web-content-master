8f48c7c63fb9bfd58eaf5c218d9a28d7b37e7713
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2017-10-12 15:53:03 +0200

    Add date and time to model

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -5,13 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @Controller
 public class GreetingController {
+
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("date", provideDate());
         return "greeting";
+    }
+
+    private String provideDate() {
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        Date today = Calendar.getInstance().getTime();
+        return df.format(today);
     }
 
 }
